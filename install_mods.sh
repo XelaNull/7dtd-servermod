04b2f25f72a280bbda074dccb2baced639d07561
XelaNull <xelanull@orbitalgrooves.com>
2019-02-15 15:58:28 -0500

    Fixed mv directory Allocs Mods folder

--- install_mods.sh
+++ install_mods.sh
@@ -77,7 +77,7 @@ wget_download "http://botman.nz/Botman_Mods_A17.zip" Allocs_Bad_Company.zip extr
 wget_download "https://confluence.catalysm.net/download/attachments/1114182/CSMM_Patrons_9.1.1.zip?api=v2" CSMM_Patrons.zip extract_file
 # CSMM Map Addon for Allocs WebAndMapRendering
 wget_download "https://confluence.catalysm.net/download/attachments/1114446/map.js?version=1&modificationDate=1548000113141&api=v2&download=true" map.js && \
-mv $MODCOUNT/map.js $MODS_DIR/1/Allocs_WebAndMapRendering/webserver/js
+mv $MODCOUNT/map.js $MODS_DIR/1/Mods/Allocs_WebAndMapRendering/webserver/js
 
 # djkrose ScriptingMod
 git_clone https://github.com/djkrose/7DTD-ScriptingMod
