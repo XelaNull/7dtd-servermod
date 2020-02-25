f27663cae9c8c84e6547a7b698308a28392795f6
XelaNull <xelanull@orbitalgrooves.com>
2020-02-24 22:15:09 -0500

    1

--- index.php
+++ index.php
@@ -387,7 +387,7 @@ function mainscreen($top, $main)
     }
 
     </style> 
-    <link href="http://mottie.github.io/tablesorter/css/theme.default.css" rel="stylesheet">
+    <!--<link href="http://mottie.github.io/tablesorter/css/theme.default.css" rel="stylesheet">-->
      
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.9.1/jquery.tablesorter.min.js"></script>
