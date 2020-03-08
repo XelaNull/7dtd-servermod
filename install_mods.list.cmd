c9e51aa9fb1a16a038b09b8363749fcc20a112ed
XelaNull <xelanull@orbitalgrooves.com>
2020-03-08 12:15:49 -0400

    update

--- install_mods.list.cmd
+++ install_mods.list.cmd
@@ -6,7 +6,7 @@
 
 wget_download "http://botman.nz/Botman_Mods_A18.zip" Allocs_Bad_Company.zip extract_file # Botman
 wget_download "https://github.com/7days2mod/BadCompanySM/releases/download/v4.2.1/BCManager.zip" BCManager.zip extract_file # Bad Company Manager
-wget_download "https://github.com/Prisma501/CSMM-Patrons-Mod/archive/13.7.zip" CSMM_Patrons.zip extract_file # CSMM PatronsFor A18.2
+#wget_download "https://github.com/Prisma501/CSMM-Patrons-Mod/archive/13.7.zip" CSMM_Patrons.zip extract_file # CSMM PatronsFor A18.2
 wget_download "https://raw.githubusercontent.com/Prisma501/Allocs-Webmap-for-CPM/master/map.js" map.js && mv $MODCOUNT/map.js $MODS_DIR/2/Mods/Allocs_WebAndMapRendering/webserver/js
 git_clone https://github.com/DelStryker/Delmod.git #Delmod Modlets
 rm -rf $MODCOUNT/Delmod/ModInfo.xml $MODCOUNT/Delmod/Delmod*Combiner $MODCOUNT/Delmod/Delmod_Pack_And_Store $MODCOUNT/Delmod/Delmod_Decorations $MODCOUNT/Delmod/Delmod_Kitchen $MODCOUNT/Delmod/Delmod_Double_Door $MODCOUNT/Delmod/Delmod_Electronics $MODCOUNT/Delmod/Delmod_Tools $MODCOUNT/Delmod/Delmod_Recipes* $MODCOUNT/Delmod/Delmod_Archetypes $MODCOUNT/Delmod/Delmod_Startup*
@@ -14,7 +14,7 @@ git_clone https://github.com/XelaNull/7dtd-Origin-UI.git # Origin UI
 
 # https://7daystodie.com/forums/showthread.php?94219-Red-Eagle-LXIX-s-A17-Modlet-Collection-(UI-Blocks-Quests)
 dropbox_download "https://www.dropbox.com/s/v1eyx3qnrmr7f2p/Red%20Eagle%20LXIX%27s%20A17%20Modlet%20Collection.zip?dl=1" Red_Eagle_Modlets.zip extract_file # Red Eagle's Modlet Collection
-rm -rf $MODCOUNT/*_UIMENU_* $MODCOUNT/*UI_SkillP* $MODCOUNT/RELXIX_Blocks_PickThisUp $MODCOUNT/RELXIX_UI_ToolbeltSlotNumbers $MODCOUNT/RELXIX_UI_PlayerStats $MODCOUNT/RELXIX_UI_MenuStats $MODCOUNT/RELXIX_UI_CompassStats
+rm -rf $MODCOUNT/*_UIMENU_* $MODCOUNT/*UI_SkillP* $MODCOUNT/RELXIX_Blocks_PickThisUp $MODCOUNT/RELXIX_UI_ToolbeltSlotNumbers $MODCOUNT/RELXIX_UI_PlayerStats $MODCOUNT/RELXIX_UI_MenuStats $MODCOUNT/RELXIX_UI_CompassStats $MODCOUNT/RELXIX_UI_ZDP1_ZombieKills $MODCOUNT/RELXIX_UI_ZDP2_ZombieKillsDeaths $MODCOUNT/RELXIX_UI_ZeeNoPlayerStatNumbers $MODCOUNT/RELXIX_UI_ZeeNoPlayerXPBar
 cd $MODCOUNT; grep Successfully -ri * | awk '{print $1}' | cut -d: -f1 > ToModify; while IFS= read -r line; do sed -i '/Successfully_Loaded/d' $line; done < ToModify; cd ..
 
 wget_download "https://github.com/dmustanger/7dtd-ServerTools/releases/download/18.2.4/7dtd-ServerTools-18.2.4.zip" ServerTools.zip extract_file # ServerTools
@@ -32,8 +32,8 @@ git_clone https://github.com/XelaNull/COMPOPACK_Modlet.git
 cp $MODS_DIR/$MODCOUNT/COMPOPACK*/Data/Prefabs/* $INSTALL_DIR/Data/Prefabs/ 
 
 #git_clone "https://github.com/TSBX-7D/Modlets.git"
-git_clone "https://github.com/doughphunghus/Doughs-PunishingWeather-Core.git"
-git_clone "https://github.com/doughphunghus/Doughs-PunishingWeather-Effects-Medium"
+#git_clone "https://github.com/doughphunghus/Doughs-PunishingWeather-Core.git"
+#git_clone "https://github.com/doughphunghus/Doughs-PunishingWeather-Effects-Medium"
 git_clone "https://github.com/doughphunghus/Doughs-PunishingWeather-Survival.git"
 
 #git_clone "https://github.com/Donovan522/donovan-7d2d-modlets.git"
